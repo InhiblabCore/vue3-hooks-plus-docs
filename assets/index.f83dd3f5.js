a6070a15b9222b775219087d7d570b22405c7796
yangjie <1013588891@qq.com>
2022-04-15 15:08:33 +0800

    feat: 'vue3-hooks-plus'

--- assets/index.f83dd3f5.js
+++
@@ -1,1 +0,0 @@
-import{t as n}from"./throttle.67c96515.js";import{x as f,e as m,I as s}from"./plugin-vue_export-helper.cdf78444.js";function v(r,e){var a;const u=f(r),l=(a=e==null?void 0:e.wait)!=null?a:1e3,t=m(()=>n((...c)=>u.value([...c]),l,e));return s(()=>{t.value.cancel()}),{run:t,cancel:t.value.cancel,flush:t.value.flush}}export{v as u};
